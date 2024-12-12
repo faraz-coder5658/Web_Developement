c1294b587b2a3ea4eee328d4e5bc23464c5e2c29
faraz-coder5658 <fa8055474@gmail.com>
2024-12-12 15:03:46 +0500

    program of HTML and CSS

---
+++ HTML-CSS/JS/New folder/simple.js
@@ -0,0 +1,14 @@
+
+
+
+export default function Faraz()
+{
+       return(
+
+        <>
+        {
+            item.filter((faraz)=>{return faraz.type===man && faraz.price<=}).map(()=>{})
+        }
+        </>
+       );
+}
